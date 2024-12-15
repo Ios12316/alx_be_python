a571662cc6dcf3c679e54dbad1cba11b3d4c8531
Idowu Olakunle Samuel <idowuolakunlesamuel@gmail.com>
2024-12-15 14:35:09 -0800

    first commit

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,6 @@
 number1 = 10
 number2 = 5
-sum = number1 + number2 
+sum = number1 + number2
 difference = number1 - number2
 product = number1 * number2
 print(f"Addition of {number1} and {number2} is {sum}")
