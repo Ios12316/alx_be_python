f0659888ef109724267e8800218be73f59c25fc4
Idowu Olakunle Samuel <idowuolakunlesamuel@gmail.com>
2025-01-01 16:17:49 +0100

    remove control-flow from remote

--- control-flow/multiplication_table.py
+++
@@ -1,4 +0,0 @@
-number = 4
-for i in range (1, 11):
-    product = number * i
-    print(f"{number} * {i} = {product}")
--- control-flow/pattern_drawing.py
+++
@@ -1,9 +0,0 @@
-size = 4
-while size <= 0:
-    print("Please use a positive integer")
-row = 1
-while row <= 4:
-    for i in range(4):
-        print("*", end="")
-    print()
-    row += 1
